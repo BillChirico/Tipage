e5cbc3513cb53c139b55e262e7f1ac8d7eb31d81
Bill Chirico <billchirico@gmail.com>
2017-08-10 13:12:57 -0400

    Condense Tips and Hours models into Shift and move Shift model to Web project

---
+++ src/Tipage.Web/Models/Shift.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tipage.Web.Models
+{
+    public class Shift
+    {
+        /// <summary>
+        /// Id of the shift.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Start of the shift.
+        /// </summary>
+        public DateTimeOffset Start { get; set; }
+
+        /// <summary>
+        /// End of the shift.
+        /// </summary>
+        public DateTimeOffset End { get; set; }
+
+        /// <summary>
+        /// Total amount of cash tips.
+        /// </summary>
+        public decimal CashTips { get; set; }
+
+        /// <summary>
+        /// Total amount of credit card tips.
+        /// </summary>
+        public decimal CreditTips { get; set; }
+    }
+}
